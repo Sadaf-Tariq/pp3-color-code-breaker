8c0b23b01faa1854208337eab90b03d1e715c33f
Sadaf Tariq <sadaf.t.tarique@gmail.com>
2023-06-05 22:39:35 +0000

    Add create code function and declare color code lists

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import gspread
+import random
 from google.oauth2.service_account import Credentials
 
 SCOPE = [
@@ -13,3 +14,15 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('score')
 
 #print(SHEET.worksheet("score").get_all_values())
+color_list = ['Red', 'Green', 'Blue', 'Purple' 'Yellow', 'White']
+color_list_map = { 'R':'Red', 'G':'Green', 'B':'Black', 'P':'Purple', 'Y':'Yellow', 'W': 'White'}
+
+
+def create_color_code(color_list):
+    random_color_code = random.sample(color_list, k=3)
+    return random_color_code
+
+print(create_color_code(color_list))
+print("Enter color code: ")
+color_code = input().upper()
+print(f'You enter {color_code} : {color_list_map[color_code]}')
